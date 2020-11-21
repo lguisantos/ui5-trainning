5e00c22469cad3074eac3097528816944815106b
lucasgsantos <lucas.guimaraes0412@gmail.com>
2020-11-20 23:21:42 -0300

    [ENHANCEMENT] - Step 7

--- webapp/controllers/App.controller.js
+++ webapp/controllers/App.controller.js
@@ -1,12 +1,26 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-  "sap/m/MessageToast"
-], function (Controller, MessageToast) {
+  "sap/m/MessageToast",
+  "sap/ui/model/json/JSONModel"
+], function (Controller, MessageToast, JSONModel) {
   "use strict";
 
   return Controller.extend("sap.ui.demo.walkthrough.views.App", {
+    onInit: function () {
+
+      var oData = {
+        recipient: {
+          name: "UI5 Developer!"
+        }
+      };
+      
+      // we need to transform our object in JSONModel to put them inside of View
+      var oModel = new JSONModel(oData);
+      this.getView().setModel(oModel);
+    },
+
     onPress: function () {
-     MessageToast.show("UI5 Message Toast Module!")
+      MessageToast.show("UI5 Message Toast Module!")
     }
   })
 });
