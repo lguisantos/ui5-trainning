83d076744c9112835fb3301389e804d335712deb
lucasgsantos <lucas.guimaraes0412@gmail.com>
2020-11-20 23:41:45 -0300

    [ENHANCEMENT] - Step 8

--- webapp/controllers/App.controller.js
+++ webapp/controllers/App.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
   "sap/m/MessageToast",
-  "sap/ui/model/json/JSONModel"
-], function (Controller, MessageToast, JSONModel) {
+  "sap/ui/model/json/JSONModel",
+  "sap/ui/model/resource/ResourceModel"
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
   "use strict";
 
   return Controller.extend("sap.ui.demo.walkthrough.views.App", {
@@ -13,14 +14,27 @@ sap.ui.define([
           name: "UI5 Developer!"
         }
       };
-      
+
       // we need to transform our object in JSONModel to put them inside of View
       var oModel = new JSONModel(oData);
       this.getView().setModel(oModel);
+
+      //set i18nModel on our view
+      var i18nModel = new ResourceModel({
+        bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
+        sopportedLocales: [""],
+        fallbackLocale: ""
+      });
+      this.getView().setModel(i18nModel, "i18n")
     },
 
     onPress: function () {
-      MessageToast.show("UI5 Message Toast Module!")
+      //read message form our i18nModel
+      var oBundle = this.getView().getModel('i18n').getResourceBundle();
+      var sRecipient = this.getView().getModel().getProperty("/recipient/name")
+      // setting message inside sRecipient
+      var sMsg = oBundle.getText("helloMsg", [sRecipient])
+      MessageToast.show(sMsg)
     }
   })
 });
